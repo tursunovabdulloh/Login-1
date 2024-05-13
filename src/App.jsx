52ab85fb60581c6f8bf0756c35f7d24eb857d358
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-05-13 17:06:24 +0300

    51

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Navigate,
   Route,
   RouterProvider,
+  useNavigate,
 } from "react-router-dom";
 import "./App.css";
+import Layout from "../Layout";
+import Login from "../Pages/Login";
+import Home from "../Pages/Home";
+import SignUp from "../Pages/SignUp";
 
 export default function App() {
+  function Redirect({ childeren }) {
+    let user = JSON.parse(localStorage.getItem("user")) ?? false;
+
+    return user ? childeren : <Navigate to="/" />;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
+      <>
         <Route path="/" element={<Login />} />
-      </Route>
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/layout"
+          element={
+            <Redirect>
+              <Layout />
+            </Redirect>
+          }
+        ></Route>
+      </>
     )
   );
 
-  return <></>;
+  return <RouterProvider router={router} />;
 }
